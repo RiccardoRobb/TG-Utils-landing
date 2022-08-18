d9ca2ad9cdfba72168c3d3cde1a2515b4e00dde4
RiccardoRobb <ruberto.riccardo@gmail.com>
2022-08-18 03:58:56 +0200

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/theme.scss'
 import type { AppProps } from 'next/app'
+import head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Roboto&display=swap" rel="stylesheet" /> 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" async/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
       <body>
         <Main />
@@ -16,5 +20,3 @@ export default function Document() {
     </Html>
   )
 }
-
-// font-awesome è utile per le icone social 
